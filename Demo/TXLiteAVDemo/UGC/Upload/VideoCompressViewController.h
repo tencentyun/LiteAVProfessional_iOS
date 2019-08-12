c230c2fdac589ca0c64c0910f5b525d13271ee3e
Jie Li <lijiegm@gmail.com>
2019-08-12 11:26:53 +0800

    目录调整

---
+++ Demo/TXLiteAVDemo/UGC/Upload/VideoCompressViewController.h
@@ -0,0 +1,14 @@
+//
+//  VideoCompressViewController.h
+//  TXLiteAVDemo_Enterprise
+//
+//  Created by xiang zhang on 2018/3/29.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface VideoCompressViewController : UIViewController
+@property(nonatomic,strong) AVAsset *videoAsset;
+@end
