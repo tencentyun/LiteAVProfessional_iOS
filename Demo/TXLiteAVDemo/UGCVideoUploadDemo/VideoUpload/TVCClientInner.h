4faaa30312b41f61ba65f95931fc0616fd505ee3
abyyxwang <abyyxwang@tencent.com>
2020-09-29 23:31:47 +0800

    iOS patch版本发布。

--- Demo/TXLiteAVDemo/UGCVideoUploadDemo/VideoUpload/TVCClientInner.h
+++ Demo/TXLiteAVDemo/UGCVideoUploadDemo/VideoUpload/TVCClientInner.h
@@ -21,7 +21,7 @@
 #define kMessage        @"message"
 #define kData           @"data"
 
-#define TVCVersion @"7.8.9518"
+#define TVCVersion @"7.8.9519"
 
 #pragma mark - COS config
 //字段废弃，作为InitUploadUGC的占位字段
